d1e1801bc202eee52f1aae6e3eaf1c052dfc733a
Priya8632 <pthummar775@gmail.com>
2022-08-07 13:27:15 +0530

    snds

--- admin/exam.php
+++ admin/exam.php
@@ -5,17 +5,17 @@ include 'config.php';
 // $result = mysqli_query($conn, $query);
 
 
-// if (!isset($_SESSION['aid'])) {
-//     header('location:admin_login.php');
-// }
-// if (!isset($_SESSION['aid'])) {
-//     $_SESSION['aid'] = $_COOKIE['aid'];
-// }
+if (!isset($_SESSION['aid'])) {
+    header('location:admin_login.php');
+}
+if (!isset($_SESSION['aid'])) {
+    $_SESSION['aid'] = $_COOKIE['aid'];
+}
 
-// $id = $_SESSION['aid'];
-// $sql =  "SELECT * FROM admin WHERE admin_id ='$id'";
-// $result1 = mysqli_query($conn, $sql);
-// $mydata = mysqli_fetch_assoc($result1);
+$id = $_SESSION['aid'];
+$sql =  "SELECT * FROM admin WHERE admin_id ='$id'";
+$result1 = mysqli_query($conn, $sql);
+$mydata = mysqli_fetch_assoc($result1);
 
 $per_page = 3;
 $start = 0;
@@ -55,10 +55,10 @@ $result = mysqli_query($conn, $query);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Better Code</title>
     <style>
-    a{
-        text-decoration: none;
-    }
-</style>
+        a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 
 <body class="antialiased bg-gray-100">
@@ -71,17 +71,18 @@ $result = mysqli_query($conn, $query);
                     <a class="flex items-center text-white space-x-4" href="">
                         <i class="fas fa-user-secret me-2" style="font-size:larger;"></i>
                         <span class="text-2xl font-bold">HEALTHCARE</span>
-                    </a><hr>
+                    </a>
+                    <hr>
                     <!-- NAV LINKS -->
                     <div class="py-4 text-gray-400 space-y-1">
                         <!-- BASIC LINK -->
-                        <a href="#" class="block py-2.5 px-4 flex items-center space-x-2 bg-gray-800 text-white rounded">
+                        <a href="#" class="block py-2.5 px-4 flex items-center space-x-2 text-white rounded">
                             <i class="fas fa-tachometer-alt me-2" style="font-size:large;"></i>
                             <span>Dashboard</span>
                         </a>
                         <!-- DROPDOWN LINK -->
                         <div class="block" x-data="{open: false}">
-                            <div @click="open = !open" class="flex items-center bg-gray-800 text-white justify-between cursor-pointer py-2.5 px-4 rounded">
+                            <div @click="open = !open" class="flex items-center text-white justify-between cursor-pointer py-2.5 px-4 rounded">
                                 <div class="flex items-center space-x-2">
                                     <i class="fa-solid fa-user-doctor me-2" style="font-size:larger;"></i>
                                     <span>Doctor</span>
@@ -94,13 +95,16 @@ $result = mysqli_query($conn, $query);
                                 </svg>
                             </div>
                             <div x-show="open" class="text-sm border-l-2 border-gray-800 mx-6 my-2.5 px-2.5 flex flex-col gap-y-1">
-                                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
+                                <a href="doctor.php" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
+                                    Profile
+                                </a>
+                                <a href="specialization.php" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
                                     Specialization
                                 </a>
                             </div>
                         </div>
                         <div class="block" x-data="{open: false}">
-                            <div @click="open = !open" class="flex items-center bg-gray-800 text-white justify-between cursor-pointer py-2.5 px-4 rounded">
+                            <div @click="open = !open" class="flex items-center text-white justify-between cursor-pointer py-2.5 px-4 rounded">
                                 <div class="flex items-center space-x-2">
                                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
@@ -115,23 +119,23 @@ $result = mysqli_query($conn, $query);
                                 </svg>
                             </div>
                             <div x-show="open" class="text-sm border-l-2 border-gray-800 mx-6 my-2.5 px-2.5 flex flex-col gap-y-1">
-                                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
+                                <a href="appointments.php" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
                                     Appointments
                                 </a>
-                                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
+                                <a href="patient.php" class="block py-2 px-4 text-white hover:bg-gray-800 hover:text-white rounded">
                                     Patients
                                 </a>
                             </div>
                         </div>
-                        <a href="#" class="block py-2.5 px-4 flex items-center space-x-2 bg-gray-800 text-white rounded">
-                            <i class="fa-solid fa-user me-2"  style="font-size:large;"></i>
+                        <a href="users.php" class="block py-2.5 px-4 flex items-center space-x-2 text-white rounded">
+                            <i class="fa-solid fa-user me-2" style="font-size:large;"></i>
                             <span>Users</span>
                         </a>
-                        <a href="#" class="block py-2.5 px-4 flex items-center space-x-2 bg-gray-800 text-white rounded">
+                        <a href="logout.php" class="block py-2.5 px-4 flex items-center space-x-2  text-white rounded">
                             <i class="fas fa-project-diagram me-2" style="font-size:large;"></i>
                             <span>Logout</span>
                         </a>
-                        
+
                     </div>
                 </div>
 
@@ -165,7 +169,11 @@ $result = mysqli_query($conn, $query);
             </div>
             <section class="max-w-7xl mx-auto py-4 px-5">
                 <div class="flex justify-between items-center border-b border-gray-300">
-                    <h1 class="text-2xl font-semibold pt-2 pb-6">Dashboard</h1>
+                    <h1 class="text-2xl font-semibold pt-2">Dashboard</h1>
+                    <a class="nav-link second-text fw-bold d-flex p-3" style="color:black;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <!-- <i class="fas fa-user me-2"></i> -->
+                        <img src="../img/admin.png" alt="" class="rounded-circle mr-3" height="16px" width="40px"><?php echo $mydata['email']; ?>
+                    </a>
                 </div>
 
                 <!-- STATISTICS -->
